d22b1b1bf811905caef7fbce7530713f58f02610
RiviCodes <jose.riveramz@udlap.mx>
2022-03-24 12:05:44 -0600

    Add components folder, including Navbar and Main

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <nav className="nav-bar">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" className="nav-icon" />
+      <h3 className="nav-logo_text">ReactFacts</h3>
+      <h4 className="nav-title">React Course - Project 1</h4>
+    </nav>
+  )
+}
