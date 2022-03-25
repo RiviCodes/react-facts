6ee691487828ac6b469e4398024b25e190001c10
RiviCodes <jose.riveramz@udlap.mx>
2022-03-24 22:14:38 -0600

    Add missing alt atribute

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
   return (
     <nav className="nav-bar">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" className="nav-icon" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" alt="react-logo" className="nav-icon" />
       <h3 className="nav-logo_text">ReactFacts</h3>
       <h4 className="nav-title">React Course - Project 1</h4>
     </nav>
